931c01f2a96904696bef5482c1da6d683abfbc30
Low Mei Lin <lmlmeilin@gmail.com>
2025-06-08 22:39:58 +0800

    fix App.jsx path

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import React from 'react';
 import HeroSection from './components/HeroSection/HeroSection';
 import ProductGrid from './components/ProductGrid/ProductGrid';
-import AboutUs from './components/AboutUs';
+import AboutUs from './components/AboutUs/AboutUs';
 import ContactForm from './components/ContactForm/ContactForm';
 import Footer from './components/Footer/Footer';
 
